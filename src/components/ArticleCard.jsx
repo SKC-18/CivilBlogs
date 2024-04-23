5f2882bac635159dd2e1850dca232b2e5b4d03cb
SKC-18 <Saakar18@gmail.com>
2024-04-23 11:08:43 +0545

    articlebug

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import { images } from '../constants'
 
-const ArticleCard = () => {
+import { BsCheckLg } from 'react-icons/bs'
+
+const ArticleCard = (className) => {
   return (
-    <div className='rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px]'>
-      <img src={images.post1} alt='title' />
+    <div className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className}}`}>
+      <img src={images.post1} alt='title' className='w-full object-cover object-center h-auto md:h-52 lg:h-48 xl:h-60' />
+      <div className='p-5'>
+        <h2 className='font-roboto font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px]"'>Title</h2>
+        <p className='text-dark-light mt-3 text-sm md:text-lg'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique ex vitae natus beatae perspiciatis totam repellat unde dignissimos sed exercitationem libero sapiente assumenda porro, qui quas! Natus soluta deserunt hic?</p>
+      <div className='flex justify-between flex-nowrap items-center mt-6'>
+        <div className='flex items-center gap-x-2  md:gap-x-2.5'>
+        <img
+              src={images.Logo}
+              alt="post profile"
+              className="w-9 h-9 md:w-10 md:h-10 rounded-full"
+            />
+          <div className='flex  flex-col'>
+            <h4 className='font-bold-italic text-dark-soft text-sm'>
+              Sankala KU
+            </h4>
+            <div className='flex items-center gap-2'>
+              <span className='bg-[#36B37E] w-fit bg-opacity-20 p-1.5 rounded-full'> <BsCheckLg className='w-1.5 h-1.5 tect-[#36B37E]' /></span>
+              <span className='italic text-dark-soft text-xs' >
+                Verified
+              </span>
+
+            </div>
+          </div>
+        </div>
+        <span className='font-bold text-dark-light italic text-sm md:text-base'>Time</span>
+      </div>
+      </div>
     </div>
   )
 }
